3bcbe4ac73deb3e8d470c0739dcc202031b059a8
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-08 14:27:33 +0600

    scene and camera create

--- Lessons/Lesson-01/main.js
+++ Lessons/Lesson-01/main.js
@@ -1,1 +1,19 @@
-alert("Hey");
+console.log(THREE);
+
+let scene = THREE.Scene();
+
+let camera = THREE.PerspectiveCamera(
+  65,
+  window,
+  innerWidth / window.innerHeight,
+  0.1,
+  100,
+);
+
+scene.add(camera);
+
+let box = new THREE.BoxGeometry(1, 1, 1);
+let material = new THREE.MeshBasicMaterial({ color: "red" });
+let mesh = new THREE.Mesh(box, material);
+
+const canvas = document.querySelector("#3d");
